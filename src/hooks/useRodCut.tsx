dd5b082558402269b1192e5dbca6ee9aac4f5629
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2022-05-11 07:34:43 -0500

    Animation edited

--- src/hooks/useRodCut.tsx
+++ src/hooks/useRodCut.tsx
@@ -2,12 +2,16 @@ import { useEffect, useState } from 'react';
 
 export const useRodCut = () => {
   const initialN = 8;
+  const [restart, setRestart] = useState(false);
   const [n, setN] = useState<number>(initialN);
   const [loop, setLoop] = useState<number[]>([]);
   const length = [1, 2, 3, 4, 5, 6, 7, 8];
   const prices = [1, 5, 8, 9, 10, 17, 17, 20];
   useEffect(() => {
-    n !== 0 ? setTimeout(() => setN(n - 1), 700) : () => {};
-  }, [n, setN]);
+    setTimeout(() => {
+      if (!restart) n !== 0 ? setN(n - 1) : setRestart(!restart);
+      else n !== initialN ? setN(n + 1) : setRestart(!restart);
+    }, 700);
+  }, [n, restart, setN, setRestart]);
   return { length, loop, n, prices };
 };
